ecf792417dff5593cb768ac0ae09baa22f301b7f
techqar <artur.tyan33@gmail.com>
2020-12-03 18:03:40 +0200

    init commit

---
+++ src/main/java/com/techqar/springlibrary/domain/Author.java
@@ -0,0 +1,39 @@
+package com.techqar.springlibrary.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "author", catalog = "spring_learning")
+@EqualsAndHashCode(of = "id")
+@Getter @Setter
+@DynamicUpdate
+@DynamicInsert
+@SelectBeforeUpdate
+public class Author {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    private String fio;
+
+    private Date birthday;
+
+    @Basic(fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+
+    @Override
+    public String toString() {
+        return fio;
+    }
+}
