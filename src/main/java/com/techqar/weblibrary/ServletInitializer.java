2a15f6bad493f6d56771b2485e79dc813c3862df
techqar <artur.tyan33@gmail.com>
2020-12-07 15:21:53 +0200

    add dependencies for frontend, add jsf templates, add jsf configurations file, web.xml, glassfish.web.xml, add css files, images, message bundles

--- src/main/java/com/techqar/weblibrary/ServletInitializer.java
+++ src/main/java/com/techqar/weblibrary/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.techqar.weblibrary;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
--- src/main/java/com/techqar/weblibrary/spring/controller/RedirectController.java
+++ src/main/java/com/techqar/weblibrary/spring/controller/RedirectController.java
@@ -4,45 +4,18 @@ import com.techqar.weblibrary.dao.impl.AuthorService;
 import com.techqar.weblibrary.dao.impl.BookService;
 import com.techqar.weblibrary.dao.impl.GenreService;
 import com.techqar.weblibrary.dao.impl.PublisherService;
-import com.techqar.weblibrary.domain.Author;
-import com.techqar.weblibrary.domain.Book;
-import com.techqar.weblibrary.spring.repo.AuthorRepo;
-import com.techqar.weblibrary.spring.repo.BookRepo;
-import com.techqar.weblibrary.spring.repo.GenreRepo;
-import com.techqar.weblibrary.spring.repo.PublisherRepo;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Date;
-import java.util.Optional;
 
 @Controller
 @Log
 public class RedirectController {
-
-    @Autowired
-    private AuthorRepo authorRepo;
-
-    @Autowired
-    private BookRepo bookRepo;
-
-    @Autowired
-    private GenreRepo genreRepo;
-
-    @Autowired
-    private PublisherRepo publisherRepo;
 
     @Autowired
     private AuthorService authorService;
@@ -57,10 +30,7 @@ public class RedirectController {
     private PublisherService publisherService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String baseUrlRedirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-
-
-        return "ok";
+    public String baseUrlRedirect(HttpServletRequest request, HttpServletResponse response) {
+        return "redirect:" + request.getRequestURL().append("index.xhtml").toString();
     }
 }
